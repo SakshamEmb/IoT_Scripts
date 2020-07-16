26bd0ed9010f1631ff8b92881038bf5c8dbdaefb
SamEmb <40595362+SamEmb@users.noreply.github.com>
2020-07-16 10:15:49 +0530

    Update check_io.py

--- check_io.py
+++ check_io.py
@@ -10,14 +10,17 @@ n = len(io)
 i = 0 
 count = 0
 count1 = 0 
+count3 = 0 
+panic = 0
+check =0  
 def flag(x,b,count):   # detects fluctuating output 
 	x = int(x)
 	b = int(b)
 	d = count
-	#rint a,b
+	#print a,b
 	if x != b:
 		d = d + 1
-		print 'here'
+		#print 'here'
 		return d
 	else :
 		return d	
@@ -38,7 +41,7 @@ while i < n :
 	f = io[i]
 	a = str(f)
 	val = len(a)
-	print val
+	#rint val
 	if val <8:	
 		b = convert2byte(val)
 		a = b + a	
@@ -47,21 +50,33 @@ while i < n :
 	else : io1.append(0)
 	temp = io1[i]
 	if i>1 :
-		print count 
+		#print count 
 		count = flag(temp,io1[i-1],count)
 	if int(a[0]) == 1:
 		io2.append(1)
 	else : io2.append(0)
 	temp2= io2[i]
 	if i>1 :
-		print count1 
-		count1 = flag(temp,io2[i-1],count1)
-	if io1[i]==1 & io2[i]==1 : print "p0anic" 
-	i = i+1 
+		#print count1 
+		count1 = flag(temp2,io2[i-1],count1)
+	if io1[i]==1 & io2[i]==1 : check  = check + 1
+	
 
-print io1 
-print io2
-print "-----------------------------------------ROUGH WORK-------------------------------------"
-print "COUNT is ===== %s"  %(count)
-print "COUNT 2 is ===== %s"  %(count1)
+	if io1[i]==0 & count3<=1000 : 
+		count3 = count3 + 1 
+		#print count3
+	elif count3>=1000 or io2[i]==1:
+		if count3>=1000:
+			print "panic" 
+			panic = 1 
+		else: count3 = 0 	
+	i = i+1 
+	
+#print io1 
+#print io2
+#print "-----------------------------------------ROUGH WORK-------------------------------------"
+print "Ignition toggled this many times ===== %s"  %(count)
+print "Tamper Bit toggled this many times ===== %s"  %(count1)
+print "Check ===== %s"  %(check)
+print "Installation Issue ==== %s" %(panic)
 
