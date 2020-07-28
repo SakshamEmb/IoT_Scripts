9b240dde87e19f52f69619b7284a60a8bf963264
SamEmb <saksmsangwan@outlook.com>
2020-07-28 13:06:05 +0530

    Finalised the version 1 of Logic for FMB Device

--- fmb_logic.py
+++ fmb_logic.py
@@ -12,9 +12,13 @@ battery1 = filtered[filtered['speed']==0] # speed = 0
 battery2 = filtered[filtered['speed']!=0]
 battery1 = battery1["vehicleBatteryLevel"].to_numpy()
 battery2 = battery2["vehicleBatteryLevel"].to_numpy()
+io_s = filtered[filtered['speed']!=0]
+io_s = io_s["ioState"].to_numpy()
 io = []
 io1 = []
 io2 = []
+io3 = []
+io4 = []
 io = d1
 n = len(io)
 i = 0 
@@ -32,6 +36,7 @@ battcheck = 0
 error = 0  
 cnt = 0  # battery voltage over 13.2 count
 previous_v = 0 
+mcb = 0 
 def flag(x,b,count,index):   # detects fluctuating output # present value , previous value
 	x = int(x)
 	b = int(b)
@@ -46,6 +51,15 @@ def flag(x,b,count,index):   # detects fluctuating output # present value , prev
 		return d
 	else :
 		return d
+def toggle_0_1(xp,xn,zp,zn):
+	xp = int(xp)
+	xp = int(xn)
+	zp = int(zp)
+	zn= int(zn)
+
+	if xp == 0  and xn == 1 and yn == 1 and yp == 0 and mcb == 0 :
+		mcb = 1 
+
 def battcnt(voltage,i):
 	volt = voltage[i]
 	if volt>13.2:
@@ -77,39 +91,55 @@ def convert2byte(argument):
 
 while i < n :
 	ind = i
+	i1 = i
 	if i>len(battery2)-1:
 		ind = len(battery2)-1
+	if i1>len(io_s)-1:
+		i1 = len(io_s) -1 
 	#if i>len(battery1)-1:
 	#	ind = len(battery1)-1
-	f = io[i]
+	f = io[i] # All A packets 
 	a = str(f)
 	val = len(a)
-	#print(val)
+
 	if val <8:	
 		b = convert2byte(val)
 		a = b + a	
+
+	g = io_s[i1]  # A packets with speed >0
+	a1 =str(g)
+	val1 = len(a1)
+	if val1 <8:	
+		b1 = convert2byte(val1)
+		a1 = b1 + a1	
 
 	if int(a[1]) == 1:
 		io1.append(1)
 	else : io1.append(0)
 	temp = io1[i]
-	if i>1 :
-		#print count 
-		count = flag(temp,io1[i-1],count,i)
-
 
 	if int(a[0]) == 1:
 		io2.append(1)
 	else : io2.append(0)
 	temp2= io2[i]
 
-	
-	
+	if int(a1[2]) == 1 :
+		io3.append(1)
+	else : io3.append(0)
+	temp3 = io3[i1]
+
+	if int(a1[1])==1:
+		io4.append(1)
+	else : io4.append(0)	
+	temp4 = io4[i1]
+
 	if i>1 :
 		#print count1
 		#print(battery2)
+		count = flag(temp,io1[i-1],count,i)
 		count1 = flag(temp2,io2[i-1],count1,i)
 		x = count1
+		toggle_0_1(io3[i-1],temp3,io4[i-1],temp4)
 		#print(" x === %s" %(x)) 
 		battbit = battcnt(battery2,ind)
 		if i>num:
@@ -131,6 +161,11 @@ while i < n :
 							sensor = input("Is sensor connected ? ")
 							if sensor == "yes":
 								print("Mark sensor connected ")
+							else :
+								if mcb == 1 :
+									print("Mark Ignition Wire Connected")
+								else :
+									print (" Ignition Wire to be Connected")	
 					else :
 						print(" TO MANUAL CHECK ")
 						error = 1 	
